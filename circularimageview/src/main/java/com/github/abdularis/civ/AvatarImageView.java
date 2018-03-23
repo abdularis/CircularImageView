1112f0693ca4fd3b4ca7d69ae64165ef4bf48035
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-03-23 09:49:15 +0700

    Initial commit

---
+++ circularimageview/src/main/java/com/github/abdularis/civ/AvatarImageView.java
@@ -0,0 +1,155 @@
+package com.github.abdularis.civ;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+
+/**
+ * Created by abdularis on 22/03/18.
+ */
+
+public class AvatarImageView extends CircleImageView {
+
+    public static final int SHOW_INITIAL_LETTER = 1;
+    public static final int SHOW_IMAGE = 2;
+
+    private static final String DEF_INITIAL_LETTER = "A";
+    private static final int DEF_TEXT_SIZE = 90;
+    private static final int DEF_BACKGROUND_COLOR = 0xE53935;
+    private static final int DEF_STATE = SHOW_INITIAL_LETTER;
+
+    private Paint mTextPaint;
+    private Rect mTextBounds;
+
+    private Paint mBackgroundPaint;
+    private RectF mBackgroundBounds;
+
+    private String mInitialLetter;
+
+    private int mShowState;
+
+    public AvatarImageView(Context context) {
+        this(context, null);
+    }
+
+    public AvatarImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        String initWord = DEF_INITIAL_LETTER;
+        int textColor = Color.WHITE;
+        int textSize = DEF_TEXT_SIZE;
+        int backgroundColor = DEF_BACKGROUND_COLOR;
+        int showState = DEF_STATE;
+
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageView, 0, 0);
+
+            initWord = a.getString(R.styleable.AvatarImageView_avatar_text);
+            textColor = a.getColor(R.styleable.AvatarImageView_avatar_textColor, textColor);
+            textSize = a.getDimensionPixelSize(R.styleable.AvatarImageView_avatar_textSize, textSize);
+            backgroundColor = a.getColor(R.styleable.AvatarImageView_avatar_backgroundColor, backgroundColor);
+            showState = a.getInt(R.styleable.AvatarImageView_avatar_state, showState);
+
+            a.recycle();
+        }
+
+        mShowState = showState;
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setColor(textColor);
+        mTextPaint.setTextSize(textSize);
+
+        mTextBounds = new Rect();
+        mInitialLetter = getFirstLetter(initWord);
+        updateTextBounds();
+
+        mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBackgroundPaint.setColor(backgroundColor);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+
+        mBackgroundBounds = new RectF();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBackgroundBounds.set(0, 0, getWidth(), getHeight());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mShowState == SHOW_INITIAL_LETTER) {
+            float textBottom = mBackgroundBounds.centerY() - mTextBounds.exactCenterY();
+            canvas.drawOval(mBackgroundBounds, mBackgroundPaint);
+            canvas.drawText(mInitialLetter, mBackgroundBounds.centerX(), textBottom, mTextPaint);
+            drawStroke(canvas);
+        } else {
+            super.onDraw(canvas);
+        }
+    }
+
+    public String getIntialLetter() {
+        return mInitialLetter;
+    }
+
+    public void setInitialLetter(String letter) {
+        mInitialLetter = getFirstLetter(letter);
+        updateTextBounds();
+        invalidate();
+    }
+
+    public int getState() {
+        return mShowState;
+    }
+
+    public void setState(int state) {
+        if (state != SHOW_INITIAL_LETTER && state != SHOW_IMAGE) {
+            String msg = "Illegal avatar state value: " + state + ", use either SHOW_INITIAL_LETTER or SHOW_IMAGE constant";
+            throw new IllegalArgumentException(msg);
+        }
+        mShowState = state;
+        invalidate();
+    }
+
+    public float getLetterSize() {
+        return mTextPaint.getTextSize();
+    }
+
+    public void setLetterSize(float size) {
+        mTextPaint.setTextSize(size);
+        updateTextBounds();
+        invalidate();
+    }
+
+    public int getTextColor() {
+        return mTextPaint.getColor();
+    }
+
+    public void setTextColor(int color) {
+        mTextPaint.setColor(color);
+        invalidate();
+    }
+
+    public int getAvatarBackgroundColor() {
+        return mBackgroundPaint.getColor();
+    }
+
+    public void setAvatarBackgroundColor(int color) {
+        mBackgroundPaint.setColor(color);
+        invalidate();
+    }
+
+    private String getFirstLetter(String letter) {
+        if (letter == null || letter.trim().length() <= 0) return "?";
+        return String.valueOf(letter.charAt(0));
+    }
+
+    private void updateTextBounds() {
+        mTextPaint.getTextBounds(mInitialLetter, 0, mInitialLetter.length(), mTextBounds);
+    }
+}
---
+++ circularimageview/src/main/java/com/github/abdularis/civ/CircleImageView.java
@@ -0,0 +1,192 @@
+package com.github.abdularis.civ;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class CircleImageView extends AppCompatImageView {
+
+    private Shader mBitmapShader;
+    private Matrix mShaderMatrix;
+
+    private RectF mBitmapDrawBounds;
+    private RectF mStrokeBounds;
+
+    private Bitmap mBitmap;
+
+    private Paint mBitmapPaint;
+    private Paint mStrokePaint;
+
+    private boolean mInitialized;
+
+    public CircleImageView(Context context) {
+        this(context, null);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        int strokeColor = Color.TRANSPARENT;
+        float strokeWidth = 0;
+
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView, 0, 0);
+
+            strokeColor = a.getColor(R.styleable.CircleImageView_strokeColor, Color.TRANSPARENT);
+            strokeWidth = a.getDimensionPixelSize(R.styleable.CircleImageView_strokeWidth, 0);
+
+            a.recycle();
+        }
+
+        mShaderMatrix = new Matrix();
+        mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mStrokeBounds = new RectF();
+        mBitmapDrawBounds = new RectF();
+        mStrokePaint.setColor(strokeColor);
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setStrokeWidth(strokeWidth);
+
+        mInitialized = true;
+
+        setupBitmap();
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        setupBitmap();
+    }
+
+    @Override
+    public void setImageDrawable(@Nullable Drawable drawable) {
+        super.setImageDrawable(drawable);
+        setupBitmap();
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        setupBitmap();
+    }
+
+    @Override
+    public void setImageURI(@Nullable Uri uri) {
+        super.setImageURI(uri);
+        setupBitmap();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        float halfStrokeWidth = mStrokePaint.getStrokeWidth() / 2f;
+        mBitmapDrawBounds.set(0, 0, getWidth(), getHeight());
+        mStrokeBounds.set(mBitmapDrawBounds);
+        mStrokeBounds.inset(halfStrokeWidth, halfStrokeWidth);
+
+        updateBitmapSize();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawBitmap(canvas);
+        drawStroke(canvas);
+    }
+
+    public int getStrokeColor() {
+        return mStrokePaint.getColor();
+    }
+
+    public void setStrokeColor(int color) {
+        mStrokePaint.setColor(color);
+        invalidate();
+    }
+
+    public float getStrokeWidth() {
+        return mStrokePaint.getStrokeWidth();
+    }
+
+    public void setStrokeWidth(float width) {
+        mStrokePaint.setStrokeWidth(width);
+        invalidate();
+    }
+
+    protected void drawStroke(Canvas canvas) {
+        if (mStrokePaint.getStrokeWidth() > 0f) {
+            canvas.drawOval(mStrokeBounds, mStrokePaint);
+        }
+    }
+
+    protected void drawBitmap(Canvas canvas) {
+        canvas.drawOval(mBitmapDrawBounds, mBitmapPaint);
+    }
+
+    private void setupBitmap() {
+        if (!mInitialized) {
+            return;
+        }
+        mBitmap = getBitmapFromDrawable(getDrawable());
+        if (mBitmap == null) {
+            return;
+        }
+
+        mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mBitmapPaint.setShader(mBitmapShader);
+
+        updateBitmapSize();
+    }
+
+    private void updateBitmapSize() {
+        if (mBitmap == null) return;
+
+        float dx = 0;
+        float dy = 0;
+        float scale;
+
+        // scale up/down with respect to this view size and maintain aspect ratio
+        if (mBitmap.getWidth() < mBitmap.getHeight()) {
+            scale = (float)getWidth() / (float)mBitmap.getWidth();
+            dy = (mBitmap.getHeight() * scale) * 0.5f - (getHeight() * 0.5f);
+        } else {
+            scale = (float)getHeight() / (float)mBitmap.getHeight();
+            dx = (mBitmap.getWidth() * scale) * 0.5f - (getWidth() * 0.5f);
+        }
+        mShaderMatrix.setScale(scale, scale);
+        mShaderMatrix.postTranslate(-dx, -dy);
+        mBitmapShader.setLocalMatrix(mShaderMatrix);
+    }
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
