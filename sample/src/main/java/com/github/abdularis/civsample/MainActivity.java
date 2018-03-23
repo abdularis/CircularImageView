a184579045efa5eb853a625079858ba7b7377c9b
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-03-24 06:53:05 +0700

    Update sample app

--- sample/src/main/java/com/github/abdularis/civsample/MainActivity.java
+++ sample/src/main/java/com/github/abdularis/civsample/MainActivity.java
@@ -2,6 +2,8 @@ package com.github.abdularis.civsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCircleImageClick(View view) {
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
     }
 }
