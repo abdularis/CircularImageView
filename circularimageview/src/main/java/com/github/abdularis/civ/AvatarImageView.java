a8bb2e9b363f3b405bd503822efefd2809e7acd5
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-03-24 10:21:15 +0700

    Fix calculate padding of the image view

--- circularimageview/src/main/java/com/github/abdularis/civ/AvatarImageView.java
+++ circularimageview/src/main/java/com/github/abdularis/civ/AvatarImageView.java
@@ -78,7 +78,7 @@ public class AvatarImageView extends CircleImageView {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mBackgroundBounds.set(0, 0, getWidth(), getHeight());
+        mBackgroundBounds.set(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
     }
 
     @Override
